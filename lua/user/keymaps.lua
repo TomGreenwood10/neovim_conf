1f1cb4979c078d0b52f8d4eea16eab2960e2c6b3
Tom Greenwood <tomgreenwood10@gmail.com>
2022-11-15 19:44:18 +0000

    Initial commit basic config

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,20 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<C-k>", "<C-w>k", opts) 
+keymap("n", "<C-l>", "<C-w>l", opts) 
+keymap("n", "<C-j>", "<C-w>j", opts) 
+keymap("n", "<C-h>", "<C-w>h", opts) 
+
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- TODO: below doesn't work
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
---
+++ lua/user/options.lua
@@ -0,0 +1,5 @@
+-- :help options
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.shiftwidth = 4
+vim.opt.number = true
