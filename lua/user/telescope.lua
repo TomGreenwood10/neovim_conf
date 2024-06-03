ca34510d23e13b8450691abbaf1b8ca222070ab5
Tom Greenwood <tom@advancinganalytics.co.uk>
2024-06-03 11:56:18 +0100

    telescope open vsplit changed to ctrl-b to work with windows

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,12 +1,17 @@
 require('telescope').setup{
   defaults = {
     layout_strategy = 'center',
-    layout_config = { 
+    layout_config = {
       anchor = "N",
       height = 0.3,
       mirror = true,
       prompt_position = "top",
       width = 0.7,
+    },
+    mappings = {
+      i = {
+	["<C-b>"] = "select_vertical"
+      }
     },
   },
 }
